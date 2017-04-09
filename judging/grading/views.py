ffe27785eabb392ce67ad80e8b603005555fd862
Naveen Raman <dsfan414@gmail.com>
2017-04-09 16:33:17 -0400

    Able to write files to submission folder

--- judging/grading/views.py
+++ judging/grading/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import os
+
 
 # Create your views here.
 
@@ -12,4 +14,12 @@ def problem(request):
 	return render(request,'grading/problem.html', context)
 
 def submit(request):
+	fileName = request.POST['name']
+	f = request.FILES['submission']
+	with open("../submissions/"+fileName, 'wb') as destination:
+		for chunk in f.chunks():
+			destination.write(chunk)
+
+	os.system("cd ../submissions \n python3 "+fileName)
+
 	return HttpResponse(request.FILES['submission'].read())

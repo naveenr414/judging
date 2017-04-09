9bbb9eb7ef6a871d3d06f340617acc725321445b
Naveen Raman <dsfan414@gmail.com>
2017-04-09 18:16:10 -0400

    Able to verify solutions

--- judging/grading/views.py
+++ judging/grading/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 import os
-
+import time
 
 # Create your views here.
 
@@ -16,10 +16,24 @@ def problem(request):
 def submit(request):
 	fileName = request.POST['name']
 	f = request.FILES['submission']
-	with open("../submissions/"+fileName, 'wb') as destination:
+	with open("submissions/"+fileName+".py", 'wb') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
 
-	os.system("cd ../submissions \n python3 "+fileName)
+	start = time.time()
+	os.system("cd submissions \n python3 "+fileName+".py")
+	end = time.time()-start
+
+	os.system("cd ../")
+
+	if(end>=6):
+		return HttpResponse('Your response took too long')
+	else:
+		realFile = open("answers/"+fileName+".out").read()
+		theirs = open("submissions/"+fileName+".out").read()
+		if(theirs == realFile):
+			return HttpResponse("Nice job, you solved it")
+		else:
+			return HttpResponse("Wrong Answer")
 
 	return HttpResponse(request.FILES['submission'].read())
--- judging/judging/secret/__init__.py
+++ judging/judging/secret/__init__.py
@@ -1,1 +1,1 @@
-SECRET_KEY = 'amz&7l3^50xd0rq+6do3v$hg&kanl=d&lq%k=d1@%k*_uzg(7k'
+SECRET_KEY = 'amz&7l3^50xd0rq+6go1v$hg^kanl=d&lq%k=d1@%k*_uzg(7k'

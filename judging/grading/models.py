0dd60f8b481cab8099e00e45476f042efb4914f2
Naveen Raman <dsfan414@gmail.com>
2017-04-09 14:11:25 -0400

    Created the Submission and Response models

--- judging/grading/models.py
+++ judging/grading/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Submission(models.Model):
+	submission_time = models.DateTimeField("Date Added")
+	submission_location = models.CharField(max_length=200)
+	user = models.CharField(max_length=200)
+
+class Response(models.Model):
+	submissionID = models.ForeignKey(Submission,on_delete=models.CASCADE)
+	correct = models.BooleanField(default=False)
--- judging/judging/settings.py
+++ judging/judging/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'amz&7l3^50xd0rq+6do3v$hg&kanl=d&lq%k=d1@%k*_uzg(7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grading.apps.GradingConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 

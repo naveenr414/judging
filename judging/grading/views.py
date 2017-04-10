434ffb06ad92e4955287e47e21c534db00a33401
Naveen Raman <dsfan414@gmail.com>
2017-04-09 21:28:03 -0400

    Added relative directory for file opening in grading

--- judging/grading/views.py
+++ judging/grading/views.py
@@ -16,21 +16,25 @@ def problem(request):
 def submit(request):
 	fileName = request.POST['name']
 	f = request.FILES['submission']
-	with open("submissions/"+fileName+".py", 'wb') as destination:
+
+	preceder = ""
+	if("grading" in os.getcwd()):
+		preceder = "../"
+
+	with open(preceder + "submissions/"+fileName+".py", 'wb') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
 
 	start = time.time()
-	os.system("cd submissions \n python3 "+fileName+".py")
+	os.system("cd "+preceder+"submissions \n python3 "+fileName+".py")
 	end = time.time()-start
 
-	os.system("cd ../")
 
 	if(end>=6):
 		return HttpResponse('Your response took too long')
 	else:
-		realFile = open("answers/"+fileName+".out").read()
-		theirs = open("submissions/"+fileName+".out").read()
+		realFile = open(preceder+"answers/"+fileName+".out").read()
+		theirs = open(preceder+"submissions/"+fileName+".out").read()
 		if(theirs == realFile):
 			return HttpResponse("Nice job, you solved it")
 		else:
--- judging/judging/settings.py
+++ judging/judging/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nameless-inlet-15013.herokuapp.com']
+ALLOWED_HOSTS = ['nameless-inlet-15013.herokuapp.com','127.0.0.1']
 
 
 # Application definition
